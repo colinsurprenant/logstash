f3f1fa5b8b8e668b3a27f7132eabe834436c2521
Colin Surprenant <colin.surprenant@gmail.com>
2015-10-30 17:00:25 -0400

    remove obsolete files

--- logstash-core-event-java/lib/logstash/timestamp.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-# TODO: (colin) verify if necessary
-
-require "logstash/environment"
-require "logstash/json"
-require "forwardable"
-require "date"
-require "time"
--- logstash-core-event-java/lib/logstash/util/accessors.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-
-# TODO: (colin) verify if necessary
-
-
-require "logstash/namespace"
-require "logstash/util"
-require "thread_safe"
