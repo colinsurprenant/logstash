22b6ddd53431378ece2d427a43592f19fc14c9c2
Colin Surprenant <colin.surprenant@gmail.com>
2015-10-30 16:32:45 -0400

    add java code bootstrap

--- logstash-core-event-java/lib/logstash/event.rb
+++ logstash-core-event-java/lib/logstash/event.rb
@@ -1,9 +1,6 @@
 # encoding: utf-8
 
-# TODO: (colin) verify if necessary
-
 require "logstash/namespace"
-
 
 # transcient pipeline events for normal in-flow signaling as opposed to
 # flow altering exceptions. for now having base classes is adequate and
@@ -19,3 +16,8 @@ module LogStash
   # LogStash::SHUTDOWN is used by plugins
   SHUTDOWN = LogStash::ShutdownEvent.new
 end
+
+# for backward compatibility, require "logstash/event" is used a lots of places so let's bootstrap the
+# Java code loading from here.
+# TODO: (colin) I think we should mass replace require "logstash/event" with require "logstash-core-event"
+require "logstash-core-event"
