b81c8d58590f52849fea38592542aa346a44e182
Colin Surprenant <colin.surprenant@gmail.com>
2015-11-04 21:59:17 -0500

    define error class in ruby

--- logstash-core-event-java/lib/logstash/timestamp.rb
+++ logstash-core-event-java/lib/logstash/timestamp.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
 
 require "logstash/namespace"
-# for backward compatibility, require "logstash/event" is used a lots of places so let's bootstrap the
-# Java code loading from here.
-# TODO: (colin) I think we should mass replace require "logstash/event" with require "logstash-core-event"
 require "logstash-core-event"
+
+module LogStash
+  class TimestampParserError < StandardError; end
+end
