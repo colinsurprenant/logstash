973aecaabe7894f3b1484343a144fa5103d85dcc
Colin Surprenant <colin.surprenant@gmail.com>
2016-11-28 16:54:31 -0500

    add explicit constructor

--- logstash-core/src/test/java/org/logstash/ackedqueue/TestQueue.java
+++ logstash-core/src/test/java/org/logstash/ackedqueue/TestQueue.java
@@ -1,10 +1,17 @@
 package org.logstash.ackedqueue;
 
+import org.logstash.common.io.CheckpointIO;
+import org.logstash.common.io.PageIOFactory;
+
 import java.util.List;
 
 public class TestQueue extends Queue {
     public TestQueue(Settings settings) {
         super(settings);
+    }
+
+    public TestQueue(String dirPath, int pageCapacity, CheckpointIO checkpointIO, PageIOFactory pageIOFactory, Class elementClass, int maxUnread, int checkpointMaxWrites, int checkpointMaxAcks, int checkpointMaxInterval) {
+        super(dirPath, pageCapacity,checkpointIO, pageIOFactory, elementClass, maxUnread, checkpointMaxWrites, checkpointMaxAcks, checkpointMaxInterval);
     }
 
     public HeadPage getHeadPage() {
